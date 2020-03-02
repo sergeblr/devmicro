cc784b685fd9161b79c5d382000e53e2b287818a
sergeblr <sergeblr@mail.ru>
2020-03-02 17:15:32 +0300

    Docker-compose.yml (up - fully worked)

--- web-app-rabbit/src/main/java/com/epam/summer19/web_app_rabbit/ItemController.java
+++ web-app-rabbit/src/main/java/com/epam/summer19/web_app_rabbit/ItemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
+import java.math.BigDecimal;
 import java.util.List;
 
 
